2f21c8c8c538690368b43dc7735ac5e778cf263c
aparnabreddy <aparnabreddy26@gmail.com>
2021-08-05 02:39:04 +0530

    day11 and day12 task

--- day11-2ndaugust/03Aug2021-Aparba-day11/product.py
+++ day11-2ndaugust/03Aug2021-Aparba-day11/product.py
@@ -4,8 +4,16 @@ from datetime import date
 productslist=[]
 class ProductDetails:
     def addproductdetails(self, prod_name, prod_desc, prod_price,manufacturer,manufacturing_date,expiry_date):
-        dict1={"prod_name":prod_name,"prod_desc":prod_desc,"manufacturer":manufacturer,"manufacturing_date":manufacturing_date,"expiry_date":expiry_date}
+        dict1={"prod_name":prod_name,"prod_price":prod_price,"prod_desc":prod_desc,"manufacturer":manufacturer,"manufacturing_date":manufacturing_date,"expiry_date":expiry_date}
         productslist.append(dict1)
+
+def validation(prod_name,prod_price):
+    valname=re.match("([a-z]+)([a-z]+)*([a-z]+)*$",prod_name)
+    valprice=re.match("[0-9]{0,7}$",prod_price)
+    if valname and valprice:
+        return True
+    else:
+        return False
 obj1=ProductDetails()
 while(True):
     print("1.Add product")
@@ -17,11 +25,16 @@ while(True):
     if choice==1:
         prod_name=input("Enter name of the product: ")
         prod_desc=input("Enter product description: ")
-        prod_price=int(input("Enter the price of product: "))
+        prod_price=input("Enter the price of product: ")
         manufacturer=input("Enter manufacturer name: ")
         manufacturing_date=input("Enter date of manufacturing: ")
-        expiry_date=input("Enter expiry date YYYY-MM-DD: ")
-        obj1.addproductdetails(prod_name, prod_desc, prod_price,manufacturer,manufacturing_date,expiry_date)
+        expiry_date=input("Enter expiry date DD-MM-YYYY: ")
+        if validation(prod_name,prod_price):
+            obj1.addproductdetails(prod_name, prod_desc, prod_price,manufacturer,manufacturing_date,expiry_date)
+        else:
+            print("Invalid product name and price")
+            continue
+        
     if choice==2:
         print(productslist)
     if choice==3:
@@ -29,13 +42,11 @@ while(True):
         print(list(filter(lambda i:i["prod_name"]==pname,productslist)))
     if choice==4:
         current_time=time.localtime()
-        tday=time.strftime("%Y-%m-%d",current_time)
-        expiryprodlist=(list(filter(lambda i:i["expiry_date"]==str(tday),productslist)))    
+        today=time.strftime("%d-%m-%Y",current_time)
+        expiryprodlist=(list(filter(lambda i:i["expiry_date"]==str(today),productslist)))    
         if len(expiryprodlist)>0:
             print(expiryprodlist)
         else:
             print("No products that expire today")  
     if choice==5:
         break
-
-
--- day11-2ndaugust/03Aug2021-Aparba-day11/student_menu.py
+++ day11-2ndaugust/03Aug2021-Aparba-day11/student_menu.py
@@ -6,7 +6,20 @@ class StudentDetails:
         dict1={"total":totalmarks,"name":name,"rollno":rollno,"admin":admin,"physics":physics, "chemistrt":chemistry, "biology": biology,"maths":maths,"kannada":kannada,"english":english}
         Students_List.append(dict1)
 
+
+ # -----validation function----
+def validation(valname,valrollno,valadmin):
+    valname=re.search("^[A-Za-z]",name)
+    valrollno=re.search("^[0-9",rollno)
+    valadmin=re.search("^[0-9]",admin)
+    if valname and valrollno and valadmin:
+        return True
+    else:
+         return False
 obj1=StudentDetails()
+
+
+# -----menu------
 while(True):
     print("1.Add student")
     print("2.Display students aa API")
@@ -24,16 +37,8 @@ while(True):
         maths=int(input("Enter maths marks: "))
         kannada=int(input("Enter kannada marks: "))
         english=int(input("Enter english marks: "))
-        def validation():
-            a=re.search("^[A-Za-z]",name)
-            b=re.search("^[0-9",rollno)
-            c=re.search("^[0-9]",admin)
-            if a and b and c:
-                return[(name),int(rollno),int(admin)]
-            else:
-                print("Entered invalid details")    
-        
-        obj1.addstudentdetails(name, rollno,admin,physics,chemistry,biology,maths,kannada,english)
+        if validation(name,rollno,admin):
+            obj1.addstudentdetails(name, rollno,admin,physics,chemistry,biology,maths,kannada,english)
     if choice==2:
         print(Students_List)
     if choice==3:
